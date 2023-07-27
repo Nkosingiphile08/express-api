355f6b8e30205e9c7704368df4cc3727a09003c3
unknown <nkosingiphilejuliet@gmail.com>
2023-07-27 15:08:31 +0200

    add node_modules to gitignore file

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const greetings = {
-    'english' : 'Hello'
+    'english' : 'Hello' ,
+    'zulu' : 'Sawubona' ,
+   'xhosa' : 'Molo' 
 }
 
 app.get('/api/greet', function(req, res){
